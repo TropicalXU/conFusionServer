27e29af760b3f94a2333a6f587dc49b87baf2e65
Sean Hackett <seanhackett@Seans-MacBook-Pro.local>
2022-10-21 18:34:17 -0400

    Promo and Leader models implemented

---
+++ models/promotions.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const Schema = mongoose.Schema;
+
+const promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    iamge: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Promos = mongoose.model('Promos', promoSchema);
+module.exports = Promos;
